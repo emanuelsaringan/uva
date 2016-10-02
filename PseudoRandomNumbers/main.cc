dda82c06971af32e718ba90b02e9e24fbd63790d
Emanuel Saringan <dev@lambdageek.com>
2016-10-03 00:12:18 +0200

    #refactor

--- PseudoRandomNumbers/main.cc
+++ PseudoRandomNumbers/main.cc
@@ -6,14 +6,14 @@ using namespace std;
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-  long Z, I, M, L;
+  int Z, I, M, L;
   cin >> Z >> I >> M >> L;
   int c = 1;
   while (Z != 0 || I != 0 || M != 0 || L != 0) {
-    int next = (Z * L + I) % M;
+    L = (Z * L + I) % M;
     unordered_set<int> mem;
-    while (mem.insert(next).second) {
-      next = (Z * next + I) % M;
+    while (mem.insert(L).second) {
+      L = (Z * L + I) % M;
     }
     cout << "Case " << c++ << ": " << mem.size() << '\n';
     cin >> Z >> I >> M >> L;
